b6d18facb3c3b4112ffb1a60d92e2b864b0e7e0e
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-09-03 20:49:30 -0400

    change callback for google

--- passport/googleAuth.js
+++ passport/googleAuth.js
@@ -5,9 +5,10 @@ const User = require("../models/user")
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://keepintouch-server-production.up.railway.app/auth/google/callback",
     passReqToCallback   : true
   },
+  
   async function(request, accessToken, refreshToken, profile, done) {
     const newUser = new User(
         {
